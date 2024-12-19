306ae26c1186a588f35ab74c3aec89aca101deb5
josh <rodrigues.joshr@gmail.com>
2024-12-20 01:20:16 +0530

    routing fixes

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,11 +17,12 @@ const blog = defineCollection({
 
 const project = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		featured:z.boolean().default(false),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
