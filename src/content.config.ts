9c4e50afd4bf4b76c9587e979cdcb081035b4b2a
Joshuajrodrigues <rodrigues.joshr@gmail.com>
2025-06-18 20:14:03 +0530

    images fixes

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,14 +5,14 @@ const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
-  schema: z.object({
+  schema:({image})=> z.object({
     title: z.string(),
     type: z.string(),
     description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    heroImage: image(),
     tag: z.array(z.string()).optional(),
     blog: z.array(z.string()).optional(),
     link: z.string(),
@@ -24,12 +24,12 @@ const project = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/project", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
-  schema: z.object({
+  schema:({image})=> z.object({
     title: z.string(),
     type: z.string(),
     description: z.string(),
     featured: z.boolean().default(false),
-    src: z.string().optional(),
+    src: image(),
     tags: z.array(z.string()).optional(),
     contentBlocks: z.array(z.string()).optional(),
     caraousel: z.array(z.string()).optional(),
@@ -60,10 +60,10 @@ const recomends = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/recomends", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
-  schema: z.object({
+  schema:({image})=> z.object({
     title: z.string(),
     type: z.string(),
-    src: z.string().optional(),
+    src: image(),
   }),
 });
 export const collections = { art, blog, project, recomends };
