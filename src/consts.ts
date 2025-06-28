ab96378514ccc566e11bcc9b89d2c2e4d971544b
Joshuajrodrigues <rodrigues.joshr@gmail.com>
2025-06-28 15:00:22 +0530

    more tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Akanksha Gajankar';
+export const SITE_TITLE = 'Akanksha';
 export const SITE_DESCRIPTION = 'Akanksha Gajankar is a Goa, India based UI/UX designer. Discover her portfolio, read her design insights, and explore her artwork and stories.';
