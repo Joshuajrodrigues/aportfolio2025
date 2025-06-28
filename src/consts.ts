5570c7764e3e9b10f480d01a1d1a27237ef7be32
Joshuajrodrigues <rodrigues.joshr@gmail.com>
2025-06-28 14:32:32 +0530

    seo tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Akanksha';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome! I am Akanksha, a UI/UX designer in Goa. I am passionate about crafting intuitive experiences, and I love sharing my design adventures, stories, and art with you.';
