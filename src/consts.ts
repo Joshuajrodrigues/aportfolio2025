100a729f4cbb32e9451b62d5d2f0df8e6fcd97f0
Joshuajrodrigues <rodrigues.joshr@gmail.com>
2025-07-11 16:49:18 +0530

    fix nav

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Akanksha';
-export const SITE_DESCRIPTION = 'Akanksha Gajankar is a Goa, India based UI/UX designer. Discover her portfolio, read her design insights, and explore her artwork and stories.';
+export const SITE_DESCRIPTION = 'Akanksha Gajankar is a Goa, India based UI/UX designer and author. Discover her portfolio, read her design insights, and explore her artwork and stories.';
