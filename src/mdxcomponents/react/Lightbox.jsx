31632f714097133be644f32163ee8a157e876185
Joshuajrodrigues <rodrigues.joshr@gmail.com>
2025-07-12 12:29:44 +0530

    light box csss

--- src/mdxcomponents/react/Lightbox.jsx
+++ src/mdxcomponents/react/Lightbox.jsx
@@ -1,49 +1,79 @@
 import { useEffect } from "react";
 
-export  const Lightbox = ({ images, currentIndex, onClose, onNext, onPrev }) => {
+export const Lightbox = ({ images, currentIndex, onClose, onNext, onPrev }) => {
   useEffect(() => {
     const handleKeyDown = (e) => {
-      if (e.key === 'Escape') onClose();
-      if (e.key === 'ArrowRight') onNext();
-      if (e.key === 'ArrowLeft') onPrev();
+      if (e.key === "Escape") onClose();
+      if (e.key === "ArrowRight") onNext();
+      if (e.key === "ArrowLeft") onPrev();
     };
 
-    document.body.style.overflow = 'hidden';
-    document.addEventListener('keydown', handleKeyDown);
-    
+    document.body.style.overflow = "hidden";
+    document.addEventListener("keydown", handleKeyDown);
+
     return () => {
-      document.body.style.overflow = '';
-      document.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = "";
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, [onClose, onNext, onPrev]);
 
   return (
     <div className="lightbox-overlay" onClick={onClose}>
-      <div className="lightbox-content" onClick={e => e.stopPropagation()}>
-        <button className="lightbox-close" onClick={onClose}>
-          <svg viewBox="0 0 24 24">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-          </svg>
-        </button>
-        
-        <img 
-          src={images[currentIndex].src} 
-          alt={`Lightbox view ${currentIndex + 1}`}
-          className="lightbox-image"
-        />
-        
-        <div className="lightbox-nav">
-          <button className="nav-button prev" onClick={onPrev}>
-            <svg viewBox="0 0 24 24">
-              <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/>
+      <div className="overlay-content">
+        <div className="lightbox-content" onClick={(e) => e.stopPropagation()}>
+          <button className="lightbox-close" onClick={onClose}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 512 512"
+              xml:space="preserve"
+            >
+              <g>
+                <g>
+                  <path d="M505.943,6.058c-8.077-8.077-21.172-8.077-29.249,0L6.058,476.693c-8.077,8.077-8.077,21.172,0,29.249C10.096,509.982,15.39,512,20.683,512c5.293,0,10.586-2.019,14.625-6.059L505.943,35.306C514.019,27.23,514.019,14.135,505.943,6.058z"></path>
+                </g>
+              </g>
+              <g>
+                <g>
+                  <path d="M505.942,476.694L35.306,6.059c-8.076-8.077-21.172-8.077-29.248,0c-8.077,8.076-8.077,21.171,0,29.248l470.636,470.636c4.038,4.039,9.332,6.058,14.625,6.058c5.293,0,10.587-2.019,14.624-6.057C514.018,497.866,514.018,484.771,505.942,476.694z"></path>
+                </g>
+              </g>
             </svg>
           </button>
-          <div className="image-counter">
-            {currentIndex + 1} / {images.length}
-          </div>
-          <button className="nav-button next" onClick={onNext}>
-            <svg viewBox="0 0 24 24">
-              <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/>
+          <button className="nav-button prev" onClick={onPrev}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 477.175 477.175"
+              xml:space="preserve"
+            >
+              <g>
+                <path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path>
+              </g>
+            </svg>
+          </button>
+          <img
+            src={images[currentIndex].src}
+            alt={`Lightbox view ${currentIndex + 1}`}
+            className="lightbox-image"
+          />
+
+          <button className="nav-button next gnext gbtn" onClick={onNext}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 477.175 477.175"
+              xml:space="preserve"
+            >
+              <g>
+                <path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"></path>
+              </g>
             </svg>
           </button>
         </div>
