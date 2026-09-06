dedb6257a4dc7dccf244426e46ed0de855bc89ea
Joshuajrodrigues <rodrigues.joshr@gmail.com>
2026-09-06 16:39:18 +0530

    astro updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,12 @@ import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 
 import favicons from 'astro-favicons';
-
+import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.akankshagajankar.com',
+  site: 'https://www.akankshagajankar.com',
+  adapter: cloudflare(),
     integrations: [mdx(), sitemap(), svelte(), favicons(), react()],
 });
